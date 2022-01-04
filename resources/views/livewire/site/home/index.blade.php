e8034d80673229dd81dc909ea869b6905c97193f
Victor Nogueira <victornogu80@gmail.com>
2022-01-03 21:03:03 -0300

    Ajuste imagem de fundo quando houver

--- resources/views/livewire/site/home/index.blade.php
+++ resources/views/livewire/site/home/index.blade.php
@@ -32,7 +32,7 @@
                             @forelse($posts as $post)
 
                             <div
-                                class="swiper-slide d-flex align-items-center justify-content-end flex-column" @if ($post->image !== null) style="background:{{ asset('storage/'.$post->image) }}" @endif>
+                                class="swiper-slide d-flex align-items-center justify-content-end flex-column" style="background-image:({{ asset('storage/'.$post->image) }} ?? '')">
                                 @if ($post->image === null)
                                 <h2>{{ $post->title }}</h2>
                                 {!! $post->iframe !!}
